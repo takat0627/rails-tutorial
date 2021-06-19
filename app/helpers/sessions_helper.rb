f1fd388811f0b27ae636faecd68e326790fcec63
takat0627 <takat.fb.xavi@gmail.com>
2021-06-19 22:37:53 +0900

    セッションの導入

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    # session[:user_id]が存在しないときはnil
+    if session[:user_id]
+      # or equals
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
