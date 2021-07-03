6c22910adb0987a30dcac0b7e26e86a56e03b814
takatea <takat.fb.xavi@gmail.com>
2021-07-04 02:40:53 +0900

    ステータスフィードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,10 +85,16 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
-  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    # フォローしているユーザ，もしくは自分自身
+    # 下記だとDBへ二回も呼び出しがかかる
+    # Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    # サブセレクト
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
