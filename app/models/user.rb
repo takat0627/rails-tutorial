a29bb19cfec966e05174d8664fd0447388b74cdc
takatea <takat.fb.xavi@gmail.com>
2021-07-04 01:01:53 +0900

    受動的関係の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  # モデル関係
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships #, source: :follower 無くてもいい
 
+  # 検証関係
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
