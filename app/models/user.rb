c2dd8dc708ae69d281dc38aa5d4fced743a682ca
takatea <takat.fb.xavi@gmail.com>
2021-07-04 00:46:54 +0900

    フォロー，フォロー解除の関数とテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -84,6 +85,22 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # ユーザーをフォローする
+  def follow(other_user)
+    # フォローの末尾に自分を追加する（SQL的にはインサートするだけなはず）
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
